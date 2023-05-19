2a2e253a86fb94e0ace9a92c82d36ed5c5061f5f
Sailesh Rachabathuni <94635992+srachabathuni@users.noreply.github.com>
2023-05-19 15:10:27 -0700

    Updated tests

--- testengine.py
+++ testengine.py
@@ -1,8 +1,7 @@
 import connect4
 
 
-def test01():
-    moves = [1, 0, 2, 3, 4, 5, 6, 6, 4, 5, 3, 2, 1, 1, 2, 1, 6, 3, 2, 5, 5]
+def test_moves(moves):
     board = connect4.Board()
     board.forward(moves)
     board.print_board()
@@ -13,4 +12,14 @@ def test01():
     board.print_board()
 
 
-test01()
+def test01():
+    moves = [1, 0, 2, 3, 4, 5, 6, 6, 4, 5, 3, 2, 1, 1, 2, 1, 6, 3, 2, 5, 5]
+    test_moves(moves)
+
+
+def test02():
+    moves = [3, 3, 4]
+    test_moves(moves)
+
+
+test02()
